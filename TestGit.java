5adc0d125cc62ee5ecb8a5651662ee14641964c4
zZcy <zZcy@qq.com>
2023-01-13 15:34:15 +0800

    hot-fix commit2

--- TestGit.java
+++ TestGit.java
@@ -1,7 +1,7 @@
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("Hello,world!");
-        System.out.println("Hello,world!2");
+        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!2");
         System.out.println("Hello,world!hot-fix");
     }
 }
