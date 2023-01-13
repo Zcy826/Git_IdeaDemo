cdb6c852088e12822c9a69bbe8531d9c0114dba3
zZcy <zZcy@qq.com>
2023-01-13 16:45:43 +0800

    push test commit

--- TestGit.java
+++ TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
         System.out.println("Hello,Git!2");
         System.out.println("master test");
         System.out.println("Hot-Fix commit2");
+        System.out.println("在本地的idea中推送push");
     }
 }
