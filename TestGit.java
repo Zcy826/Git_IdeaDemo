24bcea009e57b2c340501b2854e5514b1693d6be
zZcy <zZcy@qq.com>
2023-01-13 15:40:22 +0800

    master commit2

--- TestGit.java
+++ TestGit.java
@@ -2,6 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
         System.out.println("Hello,Git!2");
-        System.out.println("Hello,world!hot-fix");
+        System.out.println("master test");
     }
 }
